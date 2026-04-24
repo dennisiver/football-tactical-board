a2180e2103d1004377964712f6c3f2518f1a530c
dennisiver <dennisiver@gmail.com>
2026-04-25 02:21:49 +0800

    Initial release: Football Tactical Board PWA
    iPad HD 足球戰術板，支援 Apple Pencil 繪圖、6/8/10/11人制陣型切換、5種預設戰術動畫。

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE = 'tactical-board-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  'https://unpkg.com/react@18.3.1/umd/react.development.js',
+  'https://unpkg.com/react-dom@18.3.1/umd/react-dom.development.js',
+  'https://unpkg.com/@babel/standalone@7.29.0/babel.min.js',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(hit => hit || fetch(e.request).then(res => {
+      if (!res || res.status !== 200 || res.type === 'opaque') return res;
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    }))
+  );
+});
